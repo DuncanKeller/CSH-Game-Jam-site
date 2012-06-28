f23c535d091dbf65e212495bae78c4cb26010000
DuncanKeller <keller.duncan@gmail.com>
2012-06-28 14:07:55 -0700

    kickin' things off

---
+++ index.php
@@ -0,0 +1,21 @@
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+
+	<div class = "sideBar">
+		<?php include "sideBar.php" ?>
+	</div>
+
+	<div class = "page" >
+
+		<div id = "headerImage" > </div>
+
+		<div class = "content">
+			<h2>About</h2>
+			<p>CSH Game Jam is a 24-hour race to build a game. Blah, blah, blah, content... mmm...</p>
+		</div>
+
+	</div>
+
+</body>
---
+++ sideBar.php
@@ -0,0 +1,9 @@
+<div class = "button" id = "home">
+	<a href="index.php">About</a>
+</div>	
+<div class = "button" id = "jams">
+	<a href="index.php">Jams</a>
+</div>
+<div class = "button" id = "fame">
+	<a href="index.php">Hall of Fame</a>
+</div>
